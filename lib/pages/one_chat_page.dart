f152d26526b190c59c77e362603ee6f0ffa587a7
maxim <kepl200@gmail.com>
2024-04-01 18:15:20 +0300

    хуй

--- lib/pages/one_chat_page.dart
+++ lib/pages/one_chat_page.dart
@@ -79,20 +79,15 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
 
       if (newMessages.isNotEmpty) {
         final currentUserID = context.read<UserCubit>().getUser()!.id;
-        final sender = context.read<UserCubit>().getUser()!.role == Role.pass ? 'Driver' : 'Passenger';
+        final sender = newMessages.last.senderName;
 
-        final latestMessage = newMessages.last;
+        final lastM = newMessages.last;
 
-        if (currentUserID != latestMessage.senderId) {
-          if (!list.any((message) =>
-          message.date == latestMessage.date &&
-              message.senderId == latestMessage.senderId &&
-              message.message == latestMessage.message)) {
-            ChatNotificationController.sendChatNotification(
-              sender: sender,
-              message: latestMessage.message,
-            );
-          }
+        if (currentUserID != lastM.senderId) {
+          ChatNotificationController.sendChatNotification(
+            sender: sender,
+            message: lastM.message,
+          );
         }
       }
     });
@@ -124,6 +119,7 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true, // Возвращаем автоматическое поднятие содержимого при открытии клавиатуры
       body: SafeArea(
         child: Column(
           children: [
@@ -139,7 +135,7 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
                   ),
                   Expanded(child: SizedBox()),
                   Text(
-                    'Чат с ${context.read<UserCubit>().getUser()!.role == Role.pass ? 'водителем' : 'пассажиром'}',
+                    'Чат с ${context.read<UserCubit>().getUser()!.role == Role.pass ? 'водителем ' : 'пассажиром'}',
                     style: h17w500Black,
                   ),
                   Expanded(child: SizedBox()),
@@ -149,124 +145,60 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
             SizedBox(height: 15),
             MyDivider(),
             SizedBox(height: 10),
-            Container(
-              height: WidgetsBinding.instance!.window.viewInsets.bottom > 20.0
-                  ? MediaQuery.of(context).size.height -
-                  MediaQuery.of(context).viewInsets.bottom -
-                  260
-                  : MediaQuery.of(context).size.height - 200,
-              child: SingleChildScrollView(
+            Expanded(
+              child: ListView.builder(
                 controller: controller,
-                child: Column(
-                  children: [
-                    ...list.map((e) => Column(
-                      crossAxisAlignment: context
-                          .read<UserCubit>()
-                          .getUser()!
-                          .id ==
-                          e.senderId
-                          ? CrossAxisAlignment.end
-                          : CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(left: 10, right: 10),
-                          alignment: context
-                              .read<UserCubit>()
-                              .getUser()!
-                              .id ==
-                              e.senderId
-                              ? Alignment.topRight
-                              : Alignment.topLeft,
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  final e = list[index];
+                  return Column(
+                    crossAxisAlignment: context.read<UserCubit>().getUser()!.id == e.senderId
+                        ? CrossAxisAlignment.end
+                        : CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 10, right: 10),
+                        alignment: context.read<UserCubit>().getUser()!.id == e.senderId
+                            ? Alignment.topRight
+                            : Alignment.topLeft,
+                        child: Text(
+                          DateTime(
+                              DateTime.fromMillisecondsSinceEpoch(e.date).year,
+                              DateTime.fromMillisecondsSinceEpoch(e.date).month,
+                              DateTime.fromMillisecondsSinceEpoch(e.date).day) ==
+                              DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day)
+                              ? DateFormat('hh:mm').format(DateTime.fromMillisecondsSinceEpoch(e.date))
+                              : DateFormat('hh:mm, dd MMM').format(DateTime.fromMillisecondsSinceEpoch(e.date)),
+                          style: h12w400BlackWithOpacity,
+                        ),
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(minWidth: 150),
+                        child: Container(
+                          margin: EdgeInsets.only(
+                              top: 5,
+                              bottom: 20,
+                              left: context.read<UserCubit>().getUser()!.id == e.senderId ? 50 : 10,
+                              right: context.read<UserCubit>().getUser()!.id == e.senderId ? 10 : 50),
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                              bottomLeft: Radius.circular(context.read<UserCubit>().getUser()!.id == e.senderId ? 25 : 0),
+                              bottomRight: Radius.circular(context.read<UserCubit>().getUser()!.id == e.senderId ? 0 : 25),
+                            ),
+                            color: context.read<UserCubit>().getUser()!.id == e.senderId ? blue : whiteGrey2,
+                          ),
                           child: Text(
-                            DateTime(
-                                DateTime.fromMillisecondsSinceEpoch(
-                                    e.date)
-                                    .year,
-                                DateTime.fromMillisecondsSinceEpoch(
-                                    e.date)
-                                    .month,
-                                DateTime.fromMillisecondsSinceEpoch(
-                                    e.date)
-                                    .day) ==
-                                DateTime(DateTime.now().year,
-                                    DateTime.now().month,
-                                    DateTime.now().day)
-                                ? DateFormat('hh:mm').format(
-                                DateTime.fromMillisecondsSinceEpoch(
-                                    e.date))
-                                : DateFormat('hh:mm, dd MMM').format(
-                                DateTime.fromMillisecondsSinceEpoch(
-                                    e.date)),
-                            style: h12w400BlackWithOpacity,
+                            e.message,
+                            style: h13w500Black.copyWith(color: context.read<UserCubit>().getUser()!.id == e.senderId ? Colors.white : null),
                           ),
                         ),
-                        ConstrainedBox(
-                          constraints: BoxConstraints(minWidth: 150),
-                          child: Container(
-                            margin: EdgeInsets.only(
-                                top: 5,
-                                bottom: 20,
-                                left: context
-                                    .read<UserCubit>()
-                                    .getUser()!
-                                    .id ==
-                                    e.senderId
-                                    ? 50
-                                    : 10,
-                                right: context
-                                    .read<UserCubit>()
-                                    .getUser()!
-                                    .id ==
-                                    e.senderId
-                                    ? 10
-                                    : 50),
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 20),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(20),
-                                topRight: Radius.circular(20),
-                                bottomLeft:
-                                Radius.circular(context
-                                    .read<UserCubit>()
-                                    .getUser()!
-                                    .id ==
-                                    e.senderId
-                                    ? 25
-                                    : 0),
-                                bottomRight: Radius.circular(context
-                                    .read<UserCubit>()
-                                    .getUser()!
-                                    .id ==
-                                    e.senderId
-                                    ? 0
-                                    : 25),
-                              ),
-                              color: context
-                                  .read<UserCubit>()
-                                  .getUser()!
-                                  .id ==
-                                  e.senderId
-                                  ? blue
-                                  : whiteGrey2,
-                            ),
-                            child: Text(
-                              e.message,
-                              style: h13w500Black.copyWith(
-                                  color: context
-                                      .read<UserCubit>()
-                                      .getUser()!
-                                      .id ==
-                                      e.senderId
-                                      ? Colors.white
-                                      : null),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )),
-                  ],
-                ),
+                      ),
+                    ],
+                  );
+                },
               ),
             ),
             SizedBox(height: 20),
@@ -274,32 +206,25 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 SizedBox(width: 5),
-                Container(
-                  width: message != ''
-                      ? MediaQuery.of(context).size.width - 70
-                      : MediaQuery.of(context).size.width - 10,
-                  child: Scrollbar(
-                    child: TextField(
-                      controller: textcontroller,
-                      onChanged: (value) => setState(() {
-                        message = value;
-                      }),
-                      scrollPadding: EdgeInsets.only(right: 4),
-                      scrollController: scrollController,
-                      maxLines: 4,
-                      minLines: WidgetsBinding.instance!.window.viewInsets.bottom >
-                          0.0
-                          ? 4
-                          : 1,
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                            borderSide:
-                            BorderSide(width: 0.5, color: Colors.grey),
-                            borderRadius: BorderRadius.circular(20)),
-                        border: OutlineInputBorder(
-                            borderSide:
-                            BorderSide(width: 0.5, color: Colors.grey),
-                            borderRadius: BorderRadius.circular(20)),
+                Expanded(
+                  child: Container(
+                    child: Scrollbar(
+                      child: TextField(
+                        controller: textcontroller,
+                        onChanged: (value) => setState(() {
+                          message = value;
+                        }),
+                        decoration: InputDecoration(
+                          hintText: 'Сообщение...', // Добавляем текст в поле ввода
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 0.5, color: Colors.grey),
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 0.5, color: Colors.grey),
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -319,9 +244,7 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
                           message: Message(
                             date: DateTime.now().millisecondsSinceEpoch,
                             senderId: context.read<UserCubit>().getUser()!.id,
-                            senderName: context.read<UserCubit>().getUser()!.fname +
-                                ' ' +
-                                context.read<UserCubit>().getUser()!.lname,
+                            senderName: context.read<UserCubit>().getUser()!.fname + ' ' + context.read<UserCubit>().getUser()!.lname,
                             message: message,
                           ),
                         );
