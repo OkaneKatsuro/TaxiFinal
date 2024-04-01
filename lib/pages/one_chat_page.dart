5e5e8b12f417a9885853bfb08447c82690ff64ff
maxim <kepl200@gmail.com>
2024-04-01 22:12:36 +0300

    хуй

--- lib/pages/one_chat_page.dart
+++ lib/pages/one_chat_page.dart
@@ -12,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../res/chat_notification_controller.dart';
 import '../res/styles.dart';
@@ -33,77 +34,29 @@ class OneChatPage extends StatefulWidget {
 class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
   late TextEditingController comment;
   late StreamSubscription<QuerySnapshot>? s;
+  List<int> sentMessagesDate = []; // Список дат отправленных сообщений
+  late DateTime lastReceivedMessageDate; // Дата и время последнего полученного сообщения
+
+  late TextEditingController textcontroller;
+  late String message = '';
+  late bool isWaiting = false;
 
   @override
   void initState() {
     super.initState();
+    lastReceivedMessageDate = DateTime.now();
     WidgetsBinding.instance?.addObserver(this);
     comment = TextEditingController(
         text: context.read<RouteFromToCubit>().get().comment ?? '');
 
-    s = FirebaseFirestore.instance
-        .collection('chats')
-        .doc('${widget.passId}-${widget.driverId}')
-        .collection('messages')
-        .snapshots()
-        .listen((querySnapshot) async {
-      print("UPDATE");
-      var listTmp = querySnapshot.docs.toList();
-      if (listTmp.length > 20) {
-        listTmp = querySnapshot.docs
-            .toList()
-            .sublist(querySnapshot.docs.toList().length - 20);
-      }
-
-      var newMessages = <Message>[];
-
-      listTmp.forEach((element) {
-        final data = element.data().values.first;
-        if (data != null) {
-          final messageData = Message.fromJson(data);
-          if (!list.any((message) =>
-          message.date == messageData.date &&
-              message.senderId == messageData.senderId &&
-              message.message == messageData.message)) {
-            newMessages.add(messageData);
-            list.add(messageData);
-          }
-        }
-      });
-
-      setState(() {});
-
-      Future.delayed(Duration(milliseconds: 500), () {
-        controller.jumpTo(controller.position.maxScrollExtent);
-      });
-
-      if (newMessages.isNotEmpty) {
-        final currentUserID = context.read<UserCubit>().getUser()!.id;
-        final sender = newMessages.last.senderName;
-
-        final lastM = newMessages.last;
-
-        if (currentUserID != lastM.senderId) {
-          ChatNotificationController.sendChatNotification(
-            sender: sender,
-            message: lastM.message,
-          );
-        }
-      }
-    });
+    textcontroller = TextEditingController();
   }
-
-  final scrollController = ScrollController();
-  String message = '';
-  bool isWaiting = false;
-  final textcontroller = TextEditingController();
-  List<Message> list = [];
-  var controller = ScrollController();
 
   @override
   void dispose() {
     WidgetsBinding.instance?.removeObserver(this);
     s?.cancel();
+    textcontroller.dispose();
     super.dispose();
   }
 
@@ -116,10 +69,22 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
     }
   }
 
+  Future<void> _loadSentMessages() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      sentMessagesDate = prefs.getStringList('sent_messages')?.map((str) => int.parse(str))?.toList() ?? [];
+    });
+  }
+
+  Future<void> _saveSentMessages() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('sent_messages', sentMessagesDate.map((date) => date.toString()).toList());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true, // Возвращаем автоматическое поднятие содержимого при открытии клавиатуры
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
         child: Column(
           children: [
@@ -146,58 +111,80 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
             MyDivider(),
             SizedBox(height: 10),
             Expanded(
-              child: ListView.builder(
-                controller: controller,
-                itemCount: list.length,
-                itemBuilder: (context, index) {
-                  final e = list[index];
-                  return Column(
-                    crossAxisAlignment: context.read<UserCubit>().getUser()!.id == e.senderId
-                        ? CrossAxisAlignment.end
-                        : CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(left: 10, right: 10),
-                        alignment: context.read<UserCubit>().getUser()!.id == e.senderId
-                            ? Alignment.topRight
-                            : Alignment.topLeft,
-                        child: Text(
-                          DateTime(
-                              DateTime.fromMillisecondsSinceEpoch(e.date).year,
-                              DateTime.fromMillisecondsSinceEpoch(e.date).month,
-                              DateTime.fromMillisecondsSinceEpoch(e.date).day) ==
-                              DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day)
-                              ? DateFormat('hh:mm').format(DateTime.fromMillisecondsSinceEpoch(e.date))
-                              : DateFormat('hh:mm, dd MMM').format(DateTime.fromMillisecondsSinceEpoch(e.date)),
-                          style: h12w400BlackWithOpacity,
-                        ),
-                      ),
-                      ConstrainedBox(
-                        constraints: BoxConstraints(minWidth: 150),
-                        child: Container(
-                          margin: EdgeInsets.only(
-                              top: 5,
-                              bottom: 20,
-                              left: context.read<UserCubit>().getUser()!.id == e.senderId ? 50 : 10,
-                              right: context.read<UserCubit>().getUser()!.id == e.senderId ? 10 : 50),
-                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(20),
-                              topRight: Radius.circular(20),
-                              bottomLeft: Radius.circular(context.read<UserCubit>().getUser()!.id == e.senderId ? 25 : 0),
-                              bottomRight: Radius.circular(context.read<UserCubit>().getUser()!.id == e.senderId ? 0 : 25),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('chats')
+                    .doc('${widget.passId}-${widget.driverId}')
+                    .collection('messages')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    List<Message> messages = snapshot.data!.docs.map((doc) {
+                      final data = doc.data() as Map<String, dynamic>;
+                      return Message.fromJson(data.values.first);
+                    }).toList();
+
+                    // Handle new messages here and send notifications if needed
+                    handleNewMessages(messages);
+
+                    return ListView.builder(
+                      itemCount: messages.length,
+                      itemBuilder: (context, index) {
+                        final e = messages[index];
+                        return Column(
+                          crossAxisAlignment: context.read<UserCubit>().getUser()!.id == e.senderId
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 10, right: 10),
+                              alignment: context.read<UserCubit>().getUser()!.id == e.senderId
+                                  ? Alignment.topRight
+                                  : Alignment.topLeft,
+                              child: Text(
+                                DateTime(
+                                    DateTime.fromMillisecondsSinceEpoch(e.date).year,
+                                    DateTime.fromMillisecondsSinceEpoch(e.date).month,
+                                    DateTime.fromMillisecondsSinceEpoch(e.date).day) ==
+                                    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day)
+                                    ? DateFormat('hh:mm').format(DateTime.fromMillisecondsSinceEpoch(e.date))
+                                    : DateFormat('hh:mm, dd MMM').format(DateTime.fromMillisecondsSinceEpoch(e.date)),
+                                style: h12w400BlackWithOpacity,
+                              ),
                             ),
-                            color: context.read<UserCubit>().getUser()!.id == e.senderId ? blue : whiteGrey2,
-                          ),
-                          child: Text(
-                            e.message,
-                            style: h13w500Black.copyWith(color: context.read<UserCubit>().getUser()!.id == e.senderId ? Colors.white : null),
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
+                            ConstrainedBox(
+                              constraints: BoxConstraints(minWidth: 150),
+                              child: Container(
+                                margin: EdgeInsets.only(
+                                    top: 5,
+                                    bottom: 20,
+                                    left: context.read<UserCubit>().getUser()!.id == e.senderId ? 50 : 10,
+                                    right: context.read<UserCubit>().getUser()!.id == e.senderId ? 10 : 50),
+                                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    topRight: Radius.circular(20),
+                                    bottomLeft: Radius.circular(context.read<UserCubit>().getUser()!.id == e.senderId ? 25 : 0),
+                                    bottomRight: Radius.circular(context.read<UserCubit>().getUser()!.id == e.senderId ? 0 : 25),
+                                  ),
+                                  color: context.read<UserCubit>().getUser()!.id == e.senderId ? blue : whiteGrey2,
+                                ),
+                                child: Text(
+                                  e.message,
+                                  style: h13w500Black.copyWith(color: context.read<UserCubit>().getUser()!.id == e.senderId ? Colors.white : null),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
                 },
               ),
             ),
@@ -215,7 +202,7 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
                           message = value;
                         }),
                         decoration: InputDecoration(
-                          hintText: 'Сообщение...', // Добавляем текст в поле ввода
+                          hintText: 'Сообщение...',
                           focusedBorder: OutlineInputBorder(
                               borderSide: BorderSide(width: 0.5, color: Colors.grey),
                               borderRadius: BorderRadius.circular(20)
@@ -230,7 +217,7 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
                   ),
                 ),
                 SizedBox(width: 5),
-                if (message != '')
+                if (message.isNotEmpty)
                   Padding(
                     padding: EdgeInsets.only(bottom: 10.0),
                     child: InkWell(
@@ -273,5 +260,30 @@ class _OneChatPageState extends State<OneChatPage> with WidgetsBindingObserver {
         ),
       ),
     );
+  }
+
+  void handleNewMessages(List<Message> messages) {
+    final currentUserID = context.read<UserCubit>().getUser()!.id;
+    final sender = messages.last.senderName;
+
+    final lastM = messages.last;
+
+    DateTime messageDateTime = DateTime.fromMillisecondsSinceEpoch(lastM.date);
+
+    if (messageDateTime.isAfter(lastReceivedMessageDate) || messageDateTime.isAtSameMomentAs(lastReceivedMessageDate)) {
+      lastReceivedMessageDate = messageDateTime;
+
+      if (currentUserID != lastM.senderId) {
+        if (!sentMessagesDate.contains(lastM.date)) {
+          ChatNotificationController.sendChatNotification(
+            sender: sender,
+            message: lastM.message,
+          );
+          sentMessagesDate.add(lastM.date);
+          _saveSentMessages();
+          print(sentMessagesDate);
+        }
+      }
+    }
   }
 }
